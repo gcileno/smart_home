f3d2e7ff30d6ede80b56eea46edc94ee3d9cea07
gcileno <gabriel.cileno.093@ufrn.edu.br>
2025-09-15 22:55:11 -0300

    LATEST(gcmc) finalizalçao da tomada

--- smart_home/dispositivos/tomada.py
+++ smart_home/dispositivos/tomada.py
@@ -1,6 +1,11 @@
-from datetime import datetime
+from asyncio import sleep
+from datetime import datetime, timedelta
 from enum import Enum, auto
+from transitions import Machine
+from smart_home.core.observers import LogEventosHub, LogRelatoriosHub
+from smart_home.core.logger import Logger
 from smart_home.dispositivos.base import Dispositivo
+from smart_home.dispositivos.luz import CorLuz
 
 class EstadoTomada(Enum):
     ON = auto()
@@ -12,6 +17,7 @@ TRANSICOES_TOMADA = [
 ]
 
 class Tomada(Dispositivo):
+
     def __init__(self, nome, estado=EstadoTomada.OFF, potencia_w: int = 0):
         super().__init__(nome, estado)
 
@@ -23,6 +29,28 @@ class Tomada(Dispositivo):
         # métricas
         self._consumo_wh = 0.0
         self._momento_ligou: datetime | None = None
+
+        #Inicializar logger
+        self.logger = Logger(
+            dispositivo=self.nome,
+            id_dispositivo=self.nome,
+            evento=None,
+            estado_origem=None,
+            estado_destino=None,
+            inicio_periodo=None,
+            fim_periodo=None,
+            total_wh=None
+        )
+        # inicializar máquina de estados
+        self.machine = Machine(
+            model=self,
+            states=[estado.name for estado in EstadoTomada],
+            transitions=TRANSICOES_TOMADA,
+            initial=EstadoTomada.OFF.name,
+            auto_transitions=False,
+            send_event=True,
+            after_state_change="atualizar_log"
+        )
 
     # --- potencia_w ---
     @property
@@ -44,22 +72,61 @@ class Tomada(Dispositivo):
             tempo_h = (datetime.now() - self._momento_ligou).total_seconds() / 3600
             return self._consumo_wh + (self._potencia_w * tempo_h)
         return self._consumo_wh
+    
+    def on_enter_ON(self, event):
+        """Ao ligar, registra o momento."""
+        self._momento_ligou = datetime.now()
+        
+        #atualizar o self.logger diretamente aqui
+        self.logger.timestamp = datetime.now()
+        self.logger.id_dispositivo = self.nome
+        self.logger.evento = 'LIGAR'
+        self.logger.estado_origem = EstadoTomada[event.transition.source]
+        self.logger.total_wh = self.consumo_wh
+        self.logger.inicio_periodo = datetime.today()
+
+        self.notificar_observadores(**self.logger.to_dict())
+    
+    def on_exit_ON(self, event):
+        """Ao desligar, acumula o consumo até aqui."""
+        if self._momento_ligou:
+            tempo_h = (datetime.now() - self._momento_ligou).total_seconds() / 3600
+            self._consumo_wh += self._potencia_w * tempo_h
+            self._momento_ligou = None
+        
+        #atualizar o self.logger diretamente aqui
+        self.logger.id_dispositivo = self.nome
+        self.logger.estado_origem = EstadoTomada[event.transition.source]
+        self.logger.estado_destino = EstadoTomada[event.transition.dest]
+        self.logger.timestamp = datetime.now()
+        self.logger.total_wh = self.consumo_wh
+        self.logger.fim_periodo = datetime.today()
+        self.notificar_observadores(**self.logger.to_dict())
+    
 
     # --- estado ---
-    @estado.setter
     def estado(self, valor):
         if not isinstance(valor, EstadoTomada):
             raise ValueError("Estado inválido: deve ser uma instância de EstadoTomada")
+        self.estado = valor
 
-        # se desligar, acumula o consumo até aqui
-        if self._estado == EstadoTomada.ON and valor == EstadoTomada.OFF:
-            if self._momento_ligou:
-                tempo_h = (datetime.now() - self._momento_ligou).total_seconds() / 3600
-                self._consumo_wh += self._potencia_w * tempo_h
-                self._momento_ligou = None
+    def atualizar_log(self, event=None):
+        self.logger.dispositivo = self.nome
+        self.logger.evento = event.event.name if event else None
+        self.logger.estado_origem = EstadoTomada[event.transition.source] if event else None
+        self.logger.estado_destino = EstadoTomada[event.transition.dest] if event else None
+        self.logger.inicio_periodo = datetime.today()
+        self.logger.fim_periodo = datetime.combine(datetime.today(), datetime.min.time()) + timedelta(days=1)
+        self.logger.total_wh = None
 
-        # se ligar, registra o momento
-        if self._estado == EstadoTomada.OFF and valor == EstadoTomada.ON:
-            self._momento_ligou = datetime.now()
 
-        self._estado = valor
+if __name__ == "__main__":
+    tom = Tomada(nome='Tomada de tv sala', estado=EstadoTomada.OFF, potencia_w=40)
+    eve = LogEventosHub()
+    rel = LogRelatoriosHub()
+    tom.adicionar_observador(eve, rel)
+    tom.potencia_w = 80
+    tom.ligar()
+    sleep(10)  # Simula 10 segundos ligado
+    tom.desligar()
+
