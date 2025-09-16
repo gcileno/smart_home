9a443ccb7102a535ab4c4d11a732c7ccb8c90326
gcileno <gabriel.cileno.093@ufrn.edu.br>
2025-09-15 23:17:36 -0300

    LATEST(gcmc) - portas finalziada

--- smart_home/dispositivos/porta.py
+++ smart_home/dispositivos/porta.py
@@ -1,4 +1,8 @@
+from datetime import datetime
 from enum import Enum, auto
+from typing import Optional
+from transitions import Machine
+from smart_home.core.logger import Logger
 from smart_home.dispositivos.base import Dispositivo
 
 class EstadoPorta(Enum):
@@ -7,11 +11,11 @@ class EstadoPorta(Enum):
     ABERTA = auto()
 
 TRANSICOES = [
-    {"trigger": "trancar", "source": EstadoPorta.DESTRANCADA, "dest": EstadoPorta.TRANCADA},
-    {"trigger": "trancar", "source": EstadoPorta.ABERTA, "dest": EstadoPorta.ABERTA, "after": "aviso_trancar_aberta"},
-    {"trigger": "destrancar", "source": EstadoPorta.TRANCADA, "dest": EstadoPorta.DESTRANCADA},
-    {"trigger": "abrir", "source": EstadoPorta.DESTRANCADA, "dest": EstadoPorta.ABERTA},
-    {"trigger": "fechar", "source": EstadoPorta.ABERTA, "dest": EstadoPorta.DESTRANCADA},
+    {"trigger": "trancar", "source": EstadoPorta.DESTRANCADA.name, "dest": EstadoPorta.TRANCADA.name},
+    {"trigger": "trancar", "source": EstadoPorta.ABERTA.name, "dest": EstadoPorta.ABERTA.name, "after": "aviso_trancar_aberta"},
+    {"trigger": "destrancar", "source": EstadoPorta.TRANCADA.name, "dest": EstadoPorta.DESTRANCADA.name},
+    {"trigger": "abrir", "source": EstadoPorta.DESTRANCADA.name, "dest": EstadoPorta.ABERTA.name},
+    {"trigger": "fechar", "source": EstadoPorta.ABERTA.name, "dest": EstadoPorta.DESTRANCADA.name},
 ]
 
 class Porta(Dispositivo):
@@ -20,15 +24,65 @@ class Porta(Dispositivo):
         super().__init__(nome, estado)
         self.__tentativas_invalidas = 0
 
-    @property
-    def estado(self):
-        return self.estado
+        self.machine = Machine(
+            model=self,
+            states=[estado.name for estado in EstadoPorta],
+            transitions=TRANSICOES,
+            initial=estado.name,
+            send_event=True,
+            after_state_change="atualizar_log"
+        )
 
-    @estado.setter
+        self.atualizar_log()
+
+    def on_enter_TRANCADA(self, event):
+        print(f"A porta {self.nome} foi trancada.")
+        self.atualizar_log(event)
+        self.notificar_observadores(**self.logger.to_dict())
+    
+    def on_enter_DESTRANCADA(self, event):
+        print(f"A porta {self.nome} foi destrancada.")
+        self.atualizar_log(event)
+        self.notificar_observadores(**self.logger.to_dict())
+    
+    def on_enter_ABERTA(self, event):
+        print(f"A porta {self.nome} foi aberta.")
+        self.atualizar_log(event)
+        self.notificar_observadores(**self.logger.to_dict())
+
     def estado(self,valor):
         if not isinstance(valor, EstadoPorta):
             raise ValueError('Estado inválido para a porta, verifique suas opções.')
         
-    def aviso_trancar_aberta(self):
+    def aviso_trancar_aberta(self, event):
         self.__tentativas_invalidas += 1
         print("Não é possível trancar pois a porta está aberta!")
+    
+    def atualizar_log(self, event: Optional[object] = None):
+
+        self.logger = Logger(
+
+            timestamp= datetime.today(),
+            dispositivo= self.nome,
+            evento= event.event.name if event else None,
+            estado_origem= EstadoPorta[event.transition.source] if event else None,
+            estado_destino= EstadoPorta[event.transition.dest] if event else None,
+            id_dispositivo= self.nome,
+            total_wh= 12,
+            inicio_periodo=datetime.today(),
+            fim_periodo=datetime.today(),
+        )
+
+if __name__ == "__main__":
+    from smart_home.core.observers import LogEventosHub, LogRelatoriosHub
+
+    eve = LogEventosHub()
+    rel = LogRelatoriosHub()
+
+    porta = Porta("Porta da Frente", EstadoPorta.TRANCADA)
+    porta.adicionar_observador(eve, rel)
+    porta.destrancar()
+    porta.abrir()
+    porta.trancar()  # Tentativa inválida, pois a porta está aberta
+    porta.fechar()
+    porta.trancar()  # Agora é válido, pois a porta está fechada
